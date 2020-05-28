38b8c41e858b801492a3704ac468036e4fe846e2
Andry <gaxeyat298@chordmi.com>
2020-05-28 16:50:01 +0300

    Updates

---
+++ precache-manifest.5cb37b1fc140e54595d49c13377df3e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e902d356710ab067e9d2244611defd",
+    "url": "/sucksV2/index.html"
+  },
+  {
+    "revision": "d99409b338fc66b8f885",
+    "url": "/sucksV2/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "e0731c4b26f347a45219",
+    "url": "/sucksV2/static/css/main.103fd6a1.chunk.css"
+  },
+  {
+    "revision": "d99409b338fc66b8f885",
+    "url": "/sucksV2/static/js/2.e601e740.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/sucksV2/static/js/2.e601e740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0731c4b26f347a45219",
+    "url": "/sucksV2/static/js/main.d8b2c947.chunk.js"
+  },
+  {
+    "revision": "4c7b83da87fc8e0c0c97",
+    "url": "/sucksV2/static/js/runtime-main.eb7a2d73.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/sucksV2/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "36ec06394b9c159b44e3384ae610d353",
+    "url": "/sucksV2/static/media/yGmxFwe.36ec0639.png"
+  }
+]);
